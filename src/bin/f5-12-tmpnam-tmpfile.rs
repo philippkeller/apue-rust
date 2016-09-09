4769747c0ff4636e9f041e5c2b98b075f99dbd39
Philipp Keller <philipp.keller@gmail.com>
2016-09-09 07:18:21 +0200

    finished code for figure 5.12

--- src/bin/f5-12-tmpnam-tmpfile.rs
+++ src/bin/f5-12-tmpnam-tmpfile.rs
@@ -1,15 +1,46 @@
+/// Code for Figure 5.12 (Demonstrate tmpnam and tmpfile functions)
+///
+/// Sad thing is that the rust code is about double the number of lines (40 lines)
+/// of the C code (20 lines), mostly because of the string conversions.
+/// It's a bit silly that there's no neat abstraction for that.
+
 extern crate libc;
 
-use std::ffi::CStr;
+use std::ffi::{CStr, CString};
+
+const MAXLINE: usize = 4096;
 
 extern {
     pub fn tmpnam(ptr: *mut libc::c_char) -> *mut libc::c_char;
 }
 
 fn main() {
-	unsafe {
+	// method 1: get pointer to new buffer	
+	let tmp = unsafe { 
 		let tmp_ptr = tmpnam(std::ptr::null_mut());
-		let tmp = CStr::from_ptr(tmp_ptr).to_owned();
-		println!("{}", tmp.into_string().unwrap());
+		CStr::from_ptr(tmp_ptr).to_owned().into_string().unwrap()
+	};
+	println!("{}", tmp);
+
+	// method 2: create buffer ourselves, make tmpnam fill this buffer
+	let tmp = unsafe {
+		let name = CString::from_vec_unchecked(Vec::with_capacity(libc::L_tmpnam as usize)).into_raw();
+		tmpnam(name);
+		CStr::from_ptr(name).to_owned().into_string().unwrap()
+	};
+	println!("{}", tmp);
+
+	unsafe {
+		let fp = libc::tmpfile();
+		if fp.is_null() {
+			panic!("tmpfile error");
+		}
+		libc::fputs(CString::new("one line of output").unwrap().as_ptr(), fp);
+		libc::rewind(fp);
+		let line = CString::from_vec_unchecked(Vec::with_capacity(MAXLINE)).into_raw();
+		if libc::fgets(line, MAXLINE as i32, fp).is_null() {
+    		panic!("fgets error");
+		}
+		println!("{}", CStr::from_ptr(line).to_owned().into_string().unwrap());
 	}
 }
