4f23c471f01fec3b4645ccb2f61476a82ca494d7
Philipp Keller <philipp.keller@gmail.com>
2016-10-04 21:00:15 +0200

    Add Figure 1.4

---
+++ src/bin/01-overview/f04-copy-stdin-stdout.rs
@@ -0,0 +1,23 @@
+extern crate libc;
+#[macro_use(as_void)]
+extern crate apue;
+
+use libc::{STDIN_FILENO, STDOUT_FILENO, read, write};
+use apue::LibcResult;
+
+const BUFSIZE:usize = 4096;
+
+fn main() {
+	unsafe {
+		let n = 0;
+		let buf:[u8;BUFSIZE] = std::mem::uninitialized();
+		while let Some(n) = read(STDIN_FILENO, as_void!(buf), BUFSIZE).to_option() {
+			if write(STDOUT_FILENO, as_void!(buf), n as _) != n as _ {
+				panic!("write error");
+			}
+		}
+		if n < 0 {
+			println!("read error");
+		}
+	}
+}
