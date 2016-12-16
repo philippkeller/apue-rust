66743bc06bdb2af33c5d040028a00acc8f82772a
Philipp Keller <philipp.keller@gmail.com>
2016-12-16 22:05:48 +0100

    Add Figure 8.17

---
+++ src/bin/08-process-cntl/f17-echo-all.rs
@@ -0,0 +1,14 @@
+/// Figure 8.17 Echo all command-line arguments and all environment strings
+///
+/// no libc, only std as it is only a helper program
+
+use std::env;
+
+fn main() {
+    for (i,arg) in env::args().enumerate() {
+        println!("argv[{}] = {}", i, arg);
+    }
+    for (key, value) in env::vars() {
+        println!("{}={}", key, value);
+    }
+}
