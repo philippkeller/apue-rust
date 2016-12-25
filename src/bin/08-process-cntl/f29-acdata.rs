1e8d49ca027aca2f5a997029a9bcc99a0dd216de
Philipp Keller <philipp.keller@gmail.com>
2016-12-25 14:36:53 +0100

    Make it work on Linux, add documentation

--- src/bin/08-process-cntl/f29-acdata.rs
+++ src/bin/08-process-cntl/f29-acdata.rs
@@ -1,13 +1,34 @@
 /// Figure 8.29 Print selected fields from system’s accounting file
 ///
-/// Status: compiles and runs on MacOs, didn't yet check if the output is correct
-/// (at least ac_comm looks good)
-
+/// Takeaway: on linux I first took acct instead of acct_v3, which had
+/// "funny" results
+///
+/// Things which could be improved in this code:
+///
+/// - use bindgen as macro directly in here instead of copy-pasting output
+///   from bindgen commandline output
+/// - maybe there's a better way to nul terminate string without doing
+///   a copy of the string
+///
+// To try this script out:
+//
+// on linux:
+// $ sudo accton /var/log/account/pacct
+// .. do a few commands
+// $ f29-acdata /var/log/account/pacct
+//
+// on mac:
+// $ sudo accton /var/account/acct
+// .. do a few commands
+// $ f29-acdata /var/account/acct
 #[macro_use(cstr)]
 extern crate apue;
 extern crate libc;
 
-use libc::{fopen, fread, c_char, c_ushort, c_uchar, c_uint, c_int, c_void, ferror};
+use libc::{fopen, fread, c_char, c_ushort, c_uint, c_void, ferror};
+#[cfg(target_os = "macos")]
+use libc::{c_uchar, c_int};
+
 use apue::{LibcResult, err_sys, array_to_string};
 
 const AFORK: u8 = 0x01; // fork'd but not exec'd
@@ -15,10 +36,16 @@ const ASU: u8 = 0x02; // used super-user permissions
 const ACORE: u8 = 0x08; // dumped core
 const AXSIG: u8 = 0x10; // killed by a signal
 
+#[cfg(target_os = "macos")]
+const COMM_LEN:usize = 10;
+#[cfg(target_os = "linux")]
+const COMM_LEN:usize = 16;
+
 // taken via bindgen from /usr/include/sys/acct.h
 #[repr(C)]
 #[derive(Copy, Clone)]
 #[derive(Debug)]
+#[cfg(target_os = "macos")]
 pub struct acct {
     pub ac_comm: [c_char; 10usize],
     pub ac_utime: c_ushort,
@@ -33,8 +60,40 @@ pub struct acct {
     pub ac_flag: c_uchar,
 }
 
+// taken via bindgen from /usr/include/linux/acct.h (acct_v3)
+#[repr(C)]
+#[derive(Copy, Clone)]
+#[derive(Debug)]
+#[cfg(target_os = "linux")]
+pub struct acct {
+    pub ac_flag: c_char,
+    pub ac_version: c_char,
+    pub ac_tty: c_ushort,
+    pub ac_exitcode: c_uint,
+    pub ac_uid: c_uint,
+    pub ac_gid: c_uint,
+    pub ac_pid: c_uint,
+    pub ac_ppid: c_uint,
+    pub ac_btime: c_uint,
+    pub ac_etime: f32,
+    pub ac_utime: c_ushort,
+    pub ac_stime: c_ushort,
+    pub ac_mem: c_ushort,
+    pub ac_io: c_ushort,
+    pub ac_rw: c_ushort,
+    pub ac_minflt: c_ushort,
+    pub ac_majflt: c_ushort,
+    pub ac_swaps: c_ushort,
+    pub ac_comm: [c_char; 16usize],
+}
+
+
 fn flag2str(acdata: acct, flag: u8, char: &str) -> &str {
-    if acdata.ac_flag & flag > 0 { char } else { " " }
+    if acdata.ac_flag as u8 & flag > 0 {
+        char
+    } else {
+        " "
+    }
 }
 
 fn main() {
@@ -50,18 +109,23 @@ fn main() {
             .expect(&format!("can't open {}", fname));
         let mut acdata: acct = std::mem::zeroed();
         while fread(&mut acdata as *mut _ as *mut c_void,
-              std::mem::size_of::<acct>(),
-              1,
-              fp) == 1 {
-            acdata.ac_comm[9] = 0; // 0-terminate string, otherwise: buffer-over-read
-            println!("{:10} e = {:8} chars = {:8} {} {} {} {}",
-                     array_to_string(&acdata.ac_comm),
+                    std::mem::size_of::<acct>(),
+                    1,
+                    fp) == 1 {
+
+            // manually nul terminate string
+            let mut ac_comm = vec![0; COMM_LEN+1];
+            ac_comm[..COMM_LEN].clone_from_slice(&acdata.ac_comm);
+
+            println!("{:16} e = {:8}, chars = {:7}, {} {} {} {}",
+                     // std::str::from_utf8(ac_comm),
+                     array_to_string(&ac_comm),
                      acdata.ac_etime,
                      acdata.ac_io,
                      flag2str(acdata, ACORE, "D"),
                      flag2str(acdata, AXSIG, "X"),
                      flag2str(acdata, AFORK, "F"),
-                     flag2str(acdata, ASU, "S"));
+                     flag2str(acdata, ASU, "S"))
         }
         if ferror(fp) != 0 {
             err_sys("read error");
