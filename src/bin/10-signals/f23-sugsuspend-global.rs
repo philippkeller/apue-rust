372fbf5e6cc99205cfd18971ed0b631fb1f21688
Philipp Keller <philipp.keller@gmail.com>
2017-01-15 22:04:57 +0100

    stuck at closure

--- src/bin/10-signals/f23-sugsuspend-global.rs
+++ src/bin/10-signals/f23-sugsuspend-global.rs
@@ -1,7 +1,12 @@
+#![feature(drop_types_in_const)]
+
 /// Figure 10.23 Using sigsuspend to wait for a global variable to be set
 ///
-/// Status: everything work except the most interesting part which is
-/// setting the static variable in sig_int and checking it in main..
+/// Status: after posting http://stackoverflow.com/questions/41655118/are-static-variables-volatile
+/// I was sure that my solution needs an Arc<>, but then I cannot initiate a Arc<> static
+/// variable as it doesn't let me do function calls. Next option: a closure which lets me
+/// reference quitflag from within sig_int, but that doesn't work as the closure signature
+/// is wrong.
 
 extern crate libc;
 extern crate apue;
@@ -11,26 +16,25 @@ use libc::{signal, sigemptyset, sigaddset};
 use apue::my_libc::{sigprocmask, sigsuspend};
 use apue::LibcResult;
 use std::sync::atomic::{AtomicBool, Ordering, ATOMIC_BOOL_INIT};
-
-// closest I could find for volatile sig_atomic_t in Rust
-static mut QUITFLAG:AtomicBool = ATOMIC_BOOL_INIT;
-
-fn sig_int(signo:c_int) {
-    unsafe {
-        match signo {
-            SIGINT => println!("\ninterrupt"),
-            SIGQUIT => {
-                println!("\nquit");
-                QUITFLAG = AtomicBool::new(true);
-            },
-            _ => panic!("unexpected signal"),
-        }
-    }
-}
+use std::sync::Arc;
 
 fn main() {
     unsafe {
         let (mut newmask, mut oldmask, mut zeromask) = std::mem::uninitialized();
+
+        // closest I could find for volatile sig_atomic_t in Rust
+        let mut quitflag:Arc<AtomicBool> = Arc::new(ATOMIC_BOOL_INIT);
+        let sig_int = |signo| {
+            match signo {
+                SIGINT => println!("\ninterrupt"),
+                SIGQUIT => {
+                    println!("\nquit");
+                    quitflag.store(true, Ordering::SeqCst);
+                },
+                _ => panic!("unexpected signal"),
+            };
+        };
+
         if signal(SIGINT, sig_int as usize) == SIG_ERR {
             panic!("signal(SIGINT) error");
         }
@@ -46,12 +50,11 @@ fn main() {
         // here comes the AtomicBool into play which goes sure that whenever
         // QUITFLAG becomes true it is immediately set to false again
         // so no other Thread could "catch" it
-        QUITFLAG = AtomicBool::new(true);
-        while QUITFLAG.fetch_or(true, Ordering::Relaxed) {
+        quitflag.store(true, Ordering::SeqCst);
+        while quitflag.fetch_or(true, Ordering::Relaxed) {
             sigsuspend(&zeromask);
             println!("exit of suspended, checking bool");
         }
-        QUITFLAG = AtomicBool::new(false);
         sigprocmask(SIG_SETMASK, &oldmask, std::ptr::null_mut()).to_option().expect("SIG_SETMASK error");
     }
 }
