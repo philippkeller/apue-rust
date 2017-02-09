27cd17bffd547a913b85d69ddc35053a451f9a9e
Philipp Keller <philipp.keller@gmail.com>
2017-02-09 17:12:18 +0100

    Finish Exercise 10.11

--- src/bin/10-signals/e11-stdin-stdout-100.rs
+++ src/bin/10-signals/e11-stdin-stdout-100.rs
@@ -11,14 +11,25 @@
 /// call setrlimit directly from the program.) Run this program on the different systems that
 /// you have access to. What happens and why?
 ///
-/// Status: compiles, did not test yet
+/// Findings:
+/// - setting RLIMIT_FSIZE on the shell via export did not trigger
+///   the exception, only with the explicit `setrlimit` call within the
+///   script is the error triggered. Also writing to a file as the solution
+///   in https://github.com/x746e/apue/blob/master/ch10/ex11-xfsz/mycat.c does
+///   did not solve the issue
+/// - somehow rust catches the error itself, so the setting of the signal
+///   has no effect. The error:
+///   thread 'main' panicked at 'failed printing to stdout: File too large (os error 27)', /Users/rustbuild/src/rust-buildbot/slave/nightly-dist-rustc-mac/build/src/libstd/io/stdio.rs:693
+///   note: Run with `RUST_BACKTRACE=1` for a backtrace.
+///   fatal runtime error: failed to initiate panic, error 5
 
 extern crate libc;
 #[macro_use(as_void)]
 extern crate apue;
 extern crate errno;
 
-use libc::{SIGXFSZ, STDIN_FILENO, STDOUT_FILENO, read, write, c_int};
+use libc::{SIGXFSZ, STDIN_FILENO, STDOUT_FILENO, c_int, rlimit, RLIMIT_FSIZE};
+use libc::{read, write, setrlimit};
 use apue::{LibcResult, signal_intr};
 use errno::errno;
 use std::io::Write;
@@ -32,15 +43,15 @@ fn main() {
     unsafe {
         signal_intr(SIGXFSZ, exceed_filesize_limit).to_option().expect("can't set SIGXFSZ");
         let mut num_loops = 0;
+        let limit = rlimit{rlim_cur: 1000, rlim_max: 1000};
+        setrlimit(RLIMIT_FSIZE, &limit);
         let buf = vec![0; buffsize];
         while let Some(n) = read(STDIN_FILENO, as_void!(buf), buffsize).to_option() {
             if write(STDOUT_FILENO, as_void!(buf), n as _) != n {
-                println!("write error");
+                println!("write error: {}", errno());
+                break;
             }
             num_loops += 1;
-        }
-        if errno().0 > 0 {
-            panic!("read error");
         }
         writeln!(&mut std::io::stderr(), "total loops: {}", num_loops).unwrap();
     }
