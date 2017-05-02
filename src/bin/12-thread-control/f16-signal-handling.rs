e4138f5b96da78a1c5ad6ed4a6746839a29cec64
Philipp Keller <philipp.keller@gmail.com>
2017-05-02 07:17:20 +0200

    Finish Figure 12.16

--- src/bin/12-thread-control/f16-signal-handling.rs
+++ src/bin/12-thread-control/f16-signal-handling.rs
@@ -1,7 +1,5 @@
 /// Figure 12.16: Synchronous signal handling
 ///
-/// Status: only partially implemented, maybe does not work yet
-///
 /// Findings:
 ///
 /// - instead of pthread_mutex_lock and pthread_cond_signal I found that
@@ -11,6 +9,13 @@
 ///   although this has been the subject before.. Interesting to see
 ///   that rusts Mutex/Condvar behave exactly the same:
 ///   https://doc.rust-lang.org/std/sync/struct.Condvar.html#examples
+//
+// Does not yet work with test.py, that's why I put it only behind two //
+// see http://stackoverflow.com/questions/43730557/
+
+// $ f16-signal-handling & pid=$! && sleep 0.1 && kill -s INT $pid && sleep 0.1 && kill -s QUIT $pid
+// interrupt
+// quit
 
 extern crate libc;
 extern crate apue;
@@ -20,8 +25,9 @@ use apue::LibcResult;
 use std::sync::{Arc, Mutex, Condvar};
 use std::thread;
 
-use libc::{sigset_t, pthread_mutex_t, SIGINT, SIGQUIT, SIG_BLOCK, PTHREAD_MUTEX_INITIALIZER};
-use libc::{sigwait, pthread_mutex_lock, pthread_mutex_unlock, sleep, exit, sigemptyset, sigaddset, pthread_sigmask};
+use libc::{sigset_t, SIGINT, SIGQUIT, SIG_BLOCK, SIG_SETMASK};
+use libc::{sigwait, exit, sigemptyset, sigaddset, pthread_sigmask};
+use apue::my_libc::sigprocmask;
 
 unsafe fn thr_fn(mask:&sigset_t, pair:Arc<(Mutex<bool>, Condvar)>) {
     let mut signo = std::mem::uninitialized();
@@ -32,6 +38,7 @@ unsafe fn thr_fn(mask:&sigset_t, pair:Arc<(Mutex<bool>, Condvar)>) {
                 println!("\ninterrupt");
             },
             SIGQUIT => {
+                println!("\nquit");
                 let &(ref lock, ref cvar) = &*pair;
                 let mut quitflag = lock.lock().unwrap();
                 *quitflag = true;
@@ -45,8 +52,6 @@ unsafe fn thr_fn(mask:&sigset_t, pair:Arc<(Mutex<bool>, Condvar)>) {
     }
 }
 
-static mut LOCK:pthread_mutex_t = PTHREAD_MUTEX_INITIALIZER;
-
 fn main() {
     unsafe {
         let pair = Arc::new((Mutex::new(false), Condvar::new()));
@@ -59,15 +64,20 @@ fn main() {
         thread::spawn(move || {
             thr_fn(&mask, pair2);
         });
-        // Wait for the thread to start up.
         let &(ref lock, ref cvar) = &*pair;
+
+        // wait for the quitflag
+        // trick is that the lock is unlocked on cvar.wait(quitflag)
         {
             let mut quitflag = lock.lock().unwrap();
             while !*quitflag {
                 quitflag = cvar.wait(quitflag).unwrap();
             }
-                * quitflag = false;
+            *quitflag = false;
         }
 
+        // SIGQUIT has been caught. Lock is unlocked upon dropping of the quitflag var
+        // reset signal mask which unblocks SIGQUIT
+        sigprocmask(SIG_SETMASK, &oldmask, std::ptr::null_mut()).check_not_negative().expect("SIG_SETMASK error");
     }
 }
